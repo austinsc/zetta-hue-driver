c992053d110b5461dee74987a69a9fddf6178d7e
Adam Magaluk <AdamMagaluK@gmail.com>
2014-07-22 10:28:11 -0400

    Updated hue bulb driver

--- hue_bulb.js
+++ hue_bulb.js
@@ -1,78 +1,82 @@
+var util = require('util');
+var Device = require('zetta').Device;
 var lightState = require("node-hue-api").lightState;
 
-var HueBulbDriver = module.exports = function(data,hue) {
-  this.type = 'huebulb';
-  this.name = 'Hue Bulb '+data.name;
-  data.hue = hue;
-  this.data = data;
-  this.hue = hue;
-  this.state = 'off';
+var HueBulb = module.exports = function(data, hue) {
+  this.bulbId = data.id;
+  this.bulbName = data.name;
+  this._hue = hue;
+  Device.call(this);
 };
+util.inherits(HueBulb, Device);
 
-HueBulbDriver.prototype.init = function(config) {
+HueBulb.prototype.init = function(config) {
   config
-    .when('on', { allow: ['turn-off', 'toggle','blink','color'] })
-    .when('off', { allow: ['turn-on', 'toggle','blink','color'] })
-    .when('blink', { allow: ['turn-on', 'toggle','blink','color'] })
+    .type('huebulb')
+    .name('Hue Hulb ' + this.bulbName)
+    .state('off')
+    .when('on', { allow: ['turn-off', 'toggle', 'blink', 'color'] })
+    .when('off', { allow: ['turn-on', 'toggle', 'blink', 'color'] })
+    .when('blink', { allow: ['turn-on', 'toggle', 'blink', 'color'] })
     .map('turn-on', this.turnOn)
     .map('turn-off', this.turnOff)
     .map('toggle', this.toggle)
-    .map('blink',this.blink)
-    .map('color',this.color,[{type:'color',name : 'color'}]);
+    .map('blink', this.blink)
+    .map('color', this.color, [{ type:'color', name: 'color' }]);
 };
 
-HueBulbDriver.prototype.blink = function(cb){
-  if(!this.hue)
+HueBulb.prototype.blink = function(cb) {
+  if (!this._hue) {
     return cb();
+  }
 
   var self = this;
-  this.hue.setLightState(this.data.id,{alert : 'select'},function(err){
+  this._hue.setLightState(this.bulbId, { alert: 'select' }, function(err){
     cb();
   });
 };
 
 
-HueBulbDriver.prototype.color = function(color,cb){
-  if(!this.hue)
+HueBulb.prototype.color = function(color, cb) {
+  if (!this._hue) {
     return cb();
+  }
 
   color = color.match(/[0-9a-f]{1,2}/g).map(function(c){ return parseInt(c,16); });
   var self = this;
-  var state = lightState.create().on().rgb(color[0],color[1],color[2]);
-  this.hue.setLightState(this.data.id,state,function(err){
+  var state = lightState.create().on().rgb(color[0], color[1], color[2]);
+  this._hue.setLightState(this.bulbId, state, function(err) {
     cb();
   });
 };
 
-HueBulbDriver.prototype.turnOn = function(cb) {
-  if(!this.hue)
+HueBulb.prototype.turnOn = function(cb) {
+  if (!this._hue) {
     return cb();
+  }
 
   var self = this;
   self.state = 'on';
   var state = lightState.create().on();
-  this.hue.setLightState(this.data.id,state,function(err){
-    if(err)
-      return cb(err);
-    cb();  
+  this._hue.setLightState(this.bulbId, state, function(err) {
+    cb();
   });
 };
 
-HueBulbDriver.prototype.turnOff = function(cb) {
-  if(!this.hue)
+HueBulb.prototype.turnOff = function(cb) {
+  if (!this._hue) {
     return cb();
+  }
 
   var self = this;
   self.state = 'off';
   var state = lightState.create().off();
-  this.hue.setLightState(this.data.id,state,function(err){
-    if(err)
-      return cb();
+  this._hue.setLightState(this.bulbId, state, function(err) {
     cb();  
   });
 };
 
-HueBulbDriver.prototype.toggle = function(cb) {
+HueBulb.prototype.toggle = function(cb) {
   if (this.state === 'off') {
     this.call('turn-on');
     cb();
