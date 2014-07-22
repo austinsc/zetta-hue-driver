ae6d4263cfbc6b08f97a03e788ad91f1b3628a0d
Adam Magaluk <AdamMagaluK@gmail.com>
2014-07-22 11:23:10 -0400

    Added base hue control class, bulb and hub only implement methods that makes sense for each type.

--- bulb.js
+++ bulb.js
@@ -1,89 +1,25 @@
 var util = require('util');
-var Device = require('zetta').Device;
 var lightState = require("node-hue-api").lightState;
+var HueDevice = require('./hue_device');
 
 var HueBulb = module.exports = function(data, hue) {
   this.bulbId = data.id;
   this.bulbName = data.name;
-  this._hue = hue;
-  Device.call(this);
+  HueDevice.call(this, hue);
 };
-util.inherits(HueBulb, Device);
+util.inherits(HueBulb, HueDevice);
 
 HueBulb.prototype.init = function(config) {
+  this._init(config);
+  
   config
-    .type('huebulb')
-    .name('Hue Hulb ' + this.bulbName)
     .state('off')
-    .when('on', { allow: ['turn-off', 'toggle', 'blink', 'color'] })
-    .when('off', { allow: ['turn-on', 'toggle', 'blink', 'color'] })
-    .when('blink', { allow: ['turn-on', 'toggle', 'blink', 'color'] })
-    .map('turn-on', this.turnOn)
-    .map('turn-off', this.turnOff)
-    .map('toggle', this.toggle)
-    .map('blink', this.blink)
-    .map('color', this.color, [{ type:'color', name: 'color' }]);
+    .type('huebulb')
+    .name('Hue Hulb ' + this.bulbName);
 };
 
-HueBulb.prototype.blink = function(cb) {
-  if (!this._hue) {
-    return cb();
-  }
-
-  var self = this;
-  this._hue.setLightState(this.bulbId, { alert: 'select' }, function(err){
-    cb();
-  });
+HueBulb.prototype.setState = function(state, cb) {
+  this._hue.setLightState(this.bulbId, state, cb);
 };
 
 
-HueBulb.prototype.color = function(color, cb) {
-  if (!this._hue) {
-    return cb();
-  }
-
-  color = color.match(/[0-9a-f]{1,2}/g).map(function(c){ return parseInt(c,16); });
-  var self = this;
-  var state = lightState.create().on().rgb(color[0], color[1], color[2]);
-  this._hue.setLightState(this.bulbId, state, function(err) {
-    cb();
-  });
-};
-
-HueBulb.prototype.turnOn = function(cb) {
-  if (!this._hue) {
-    return cb();
-  }
-
-  var self = this;
-  self.state = 'on';
-  var state = lightState.create().on();
-  this._hue.setLightState(this.bulbId, state, function(err) {
-    cb();
-  });
-};
-
-HueBulb.prototype.turnOff = function(cb) {
-  if (!this._hue) {
-    return cb();
-  }
-
-  var self = this;
-  self.state = 'off';
-  var state = lightState.create().off();
-  this._hue.setLightState(this.bulbId, state, function(err) {
-    cb();  
-  });
-};
-
-HueBulb.prototype.toggle = function(cb) {
-  if (this.state === 'off') {
-    this.call('turn-on');
-    cb();
-  } else if (this.state === 'on') {
-    this.call('turn-off');
-    cb();
-  } else {
-    cb(new Error('Invalid state - Valid states are "on" and "off".'));
-  }
-};
--- hub.js
+++ hub.js
@@ -1,53 +1,54 @@
 var util = require('util');
-var Device = require('zetta').Device;
-var HueApi = require("node-hue-api").HueApi;
-var lightState = require("node-hue-api").lightState;
+var HueApi = require('node-hue-api').HueApi;
+var HueDevice = require('./hue_device');
 
 var HueHub = module.exports = function(data) {
   this.hubId = data.id;
   this.ipAddress = data.ipaddress;
+
+  var hue = new HueApi(this.ipAddress);
   if (data.auth) {
     this.auth = data.auth
-    this._hue = new HueApi(this.ipAddress, this.auth);
+    hue = new HueApi(this.ipAddress, this.auth);
   }
-  Device.call(this);
+  HueDevice.call(this, hue);
 };
-util.inherits(HueHub, Device);
+util.inherits(HueHub, HueDevice);
 
 HueHub.prototype.init = function(config) {
-  var state = (this.auth) ? 'registered' : 'unregistered';
+  this._init(config);
+
+  var state = (this.auth) ? 'on' : 'unregistered';
   config
     .type('huehub')
     .state(state)
     .name('Hue Hub')
     .when('unregistered', { allow: ['register'] })
-    .when('registered', { allow: ['blink', 'find-lights', 'all-on', 'all-off', 'brightness', 'color', 'color-loop'] })
-    .when('colorloop', { allow: ['blink', 'find-lights', 'all-on', 'all-off', 'brightness', 'color'] })
     .map('register', this.register)
-    .map('blink', this.blink)
-    .map('all-on',this.allOn)
-    .map('all-off',this.allOff)
-    .map('find-lights',this.findLights)
-    .map('color-loop',this.colorLoop)
-    .map('color',this.color,[{type:'color', name: 'color'}])
-    .map('brightness',this.brightness,[{type: 'number', name: 'brightness'}]);
-  
+    .map('find-lights', this.findLights)
+
   if (this.auth) {
     this.findLights();
     setInterval(this.findLights.bind(this), 15000);
   }
 };
 
+HueHub.prototype.setState = function(state, cb) {
+  this._hue.setGroupLightState(0, state, cb);
+};
+
+HueHub.prototype.onDiscoveredLight = function(light) {};
+
 HueHub.prototype.register = function(cb) {
   var self = this;
   var hue = new HueApi();
   hue.createUser(this.ipAddress, null, null, function(err, user) {
     if (err) {
-      return cb(null);
+      return cb();
     }
 
     self.auth = user;
-    self.state = 'registered';
+    self.state = 'on';
     self._hue = new HueApi(self.ipAddress, self.auth);
     self.save(function() {
       self.findLights(cb);
@@ -56,87 +57,9 @@ HueHub.prototype.register = function(cb) {
   });
 };
 
-HueHub.prototype.onDiscoveredLight = function(light) {};
-
-HueHub.prototype.color = function(color,cb){
-  if(!this._hue)
-    return cb();
-  
-  color = color.match(/[0-9a-f]{1,2}/g).map(function(c){ return parseInt(c,16); });
-  var self = this;
-  var state = lightState.create().on().rgb(color[0],color[1],color[2]);
-  this._hue.setGroupLightState(0,state,function(err){
-    cb();
-  });
-};
-
-
-HueHub.prototype.blink = function(cb) {
-  if(!this._hue)
-    return cb();
-
-  var self = this;
-  self._hue.setGroupLightState(0, {alert : "select"},function(){
-    cb();
-  });
-};
-
-HueHub.prototype.allOn = function(cb) {
-  if(!this._hue)
-    return cb();
-  
-  var self = this;
-  this.data.lightval = 'on';
-  var state = lightState.create().on().brightness(100).transition(0).effect('none');
-  this._hue.setGroupLightState(0,state,function(err){
-    if(!err)
-      self.state = 'registered';
-    cb();
-  });
-};
-
-HueHub.prototype.allOff = function(cb) {
-  if (!this._hue) {
-    return cb();
-  }
-
-  var state = lightState.create().off().transition(0);
-  this._hue.setGroupLightState(0,state,function(){
-    return cb();
-  });
-};
-
-HueHub.prototype.colorLoop = function(cb) {
-  if(!this._hue)
-    return cb();
-  
-  var self = this;
-  var state = lightState.create().on().brightness(100).transition(0).effect('colorloop');
-  this._hue.setGroupLightState(0, state, function(err) {
-    if (!err) {
-      self.state = 'colorloop';
-    }
-    cb();
-  });
-};
-
-HueHub.prototype.brightness = function(brightness, cb) {
-  if (!this._hue) {
-    return cb();
-  }
-
-  var state = lightState.create().brightness(brightness);
-  this._hue.setGroupLightState(0, state, function(err) {
-    cb();
-  });
-};
-
 HueHub.prototype.findLights = function(cb) {
   if (!cb) {
     cb = function() {};
-  }
-  if (!this._hue) {
-    return cb();
   }
 
   var self = this;
